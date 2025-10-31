6480459a43cb5a852e7934aec0f921d52688c123
likhon29 <mdabdurrouf.likhon@gmail.com>
2025-10-31 20:19:28 +0600

    fix new

--- src/pages/Dashboard/ManageProjects/components/ProjectIncomeExpense.jsx
+++ src/pages/Dashboard/ManageProjects/components/ProjectIncomeExpense.jsx
@@ -1,7 +1,7 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import ProjectIncomeTable from "./ProjectIncomeTable";
-import ProjectExpenseTable from "./PRojectExpenseTable";
+import ProjectExpenseTable from "./ProjectExpenseTable";
 import { Tooltip } from "react-tooltip";
 const ProjectIncomeExpense = ({ projectIncome, projectExpense }) => {
   return (
